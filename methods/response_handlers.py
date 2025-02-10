022d12cec5a8c7e44ba83fc6ce7152e4c82c27b2
magmat <magdamatias@outlook.pt>
2025-02-10 19:38:55 +0000

    alterar sitio da resposta da madrugada

--- methods/response_handlers.py
+++ methods/response_handlers.py
@@ -52,20 +52,21 @@ async def respond_acordar(ctx: commands.Context):
     excluded = DBgeneral.get_least_favourable()
     if excluded:
         response += f" Excepto tu {excluded}! Tu podes ir pro caralho"
-    
-    #Entre as 1am e 7am o Jardas responde "Mas vocês não dormem, seus caralhos?"
-    current_time = datetime.now().time()
-    start_time = time(1, 0)
-    end_time = time(7, 0)
-    if (current_time >= start_time and current_time <= end_time):
-        response += "Mas vocês não dormem, seus caralhos? >:[ "
-    
+
     await ctx.channel.send(response)
 
 
 
 async def respond_sleep(ctx: commands.Context):
     response = random.choice(Wronged.arr_wronged)
+
+    #Entre as 1am e 7am o Jardas responde "Mas vocês não dormem, seus caralhos?"
+    current_time = datetime.now().time()
+    start_time = time(1, 0)
+    end_time = time(7, 0)
+    if (current_time >= start_time and current_time <= end_time):
+        response += "Mas vocês não dormem, seus caralhos? >:[ "
+
     await ctx.channel.send(response)
 
 
